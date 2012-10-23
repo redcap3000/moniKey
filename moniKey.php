a87accb898ed7682973896674c7bd38e4f2f6027
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2012-10-23 16:27:32 -0700

    added openshift support

--- moniKey.php
+++ moniKey.php
@@ -2,7 +2,7 @@
 
 /*
         moniKey
-        March 2012
+        October 2012
         AGPL
         Ronaldo Barbachano
 
@@ -23,19 +23,57 @@
 */
 
 // call syntax is messy but can be abstracted later..
-interface noSqlCRUD
+interface noSqlCRUD 
 {
+
+		const mongodb = 'moniKey';
+		const mongo_default_connection = 'localhost';
+		
+	//  const mongo_default_connection = 'openshift';
+	//  const mongo_default_connection = 'remote';
+			
+	//	const mongodb_remote = 'mongodb://mediator:3c0as=zZv@mdocs.net';
+	//	const mongodb_remote_db = 'noClass';
+		// application prefix (for cookies mostly)
+		const apf = 'moni';
+		const user_db = 'moni_users';
+		
+
     public function get($id,$database);
     public function put($data,$id,$database);
-// reorder and put opt before database ...
     public function view($name,$key,$database,$opt);
     public function update($id,$data,$database);
     public function delete($id,$opt,$database);
 }
+
 abstract class moniKey implements noSqlCRUD{
-        public static function __callStatic($names,$arguments){
+	public static function __callStatic($names,$arguments){
 		$names = explode('__',$names);
-		$database = self::mongodb;
+		if (self::mongo_default_connection == 'remote'){
+		// probably do isset check too ..	
+			$database = self::mongodb_remote_db;
+			$m = new Mongo(self::mongodb_remote);
+		}elseif(self::mongo_default_connection == 'openshift'){
+			// get env variables from open shift instance
+			$localhost = getenv('OPENSHIFT_NOSQL_DB_HOST');
+			$port = getenv('OPENSHIFT_NOSQL_DB_PORT');
+			$appname = getenv('OPENSHIFT_APP_NAME');
+			$username = getenv('OPENSHIFT_NOSQL_DB_USERNAME');
+			$password = getenv('OPENSHIFT_NOSQL_DB_PASSWORD');
+			// connect via mongodb url syntax
+			$m = new Mongo("mongodb://$username:$password@$localhost:$port");
+			// pre select database (based on appname, default), old school syntax to stay compatable
+			// with open shift
+			//$db = $m->selectDB($appname);
+			// select database name (should/could be same as app name) - this
+			// variable name should be collection .. but makes it a bit confusing.
+			$database = self::mongodb;
+		}else{
+			$database = self::mongodb;
+			$m = new Mongo();
+		}
+		// to determine which collection inside a mongo database to use based on the (exploded) name of the
+		// function passed to the object
 		if(count($names) ==  2)
 			$collection = $names[1];
 		elseif(count($names) == 3){
@@ -47,9 +85,10 @@ abstract class moniKey implements noSqlCRUD{
 		!$collection AND $collection = 'monikey_collection';
 		// just make a collection already if we are using outside of a 
 		//class/ do not define it to avoid massive errors consider making a class const
-		$m = new Mongo();
+		
 		// select a database
 		$db = $m->$database;
+		// select a collection
 		$collection = $db->$collection;
 		
 		if($names[0] == 'find' ){
@@ -71,8 +110,10 @@ abstract class moniKey implements noSqlCRUD{
 				// theres probably some array function that does this for me..
 				if(isset($arguments[2])){
 					// reform the array field to send back a more simple structure
-					$id = key($r);
-					$r[$id] = $r[$id][key($r[$id])];
+					foreach($r as $r_id=>$r_vals){
+						$id = key($r[$r_id]);
+						$r[$r_id] = $r[$r_id][key($r_vals)];
+					}
 				}
 				return $r;
 			}else{
@@ -90,7 +131,13 @@ abstract class moniKey implements noSqlCRUD{
 				return $r;
 			}
 			
-		}elseif($names[0] == 'findOne' && is_array($arguments[0])){
+		}elseif($names[0] == 'find_query'){
+			foreach($collection->find($arguments[0]) as $r_key=>$r_value){
+				$result[$r_key] = $r_value;
+			}
+			return $result;
+		}
+		elseif($names[0] == 'findOne' && is_array($arguments[0])){
 			if(isset($arguments[0])){
 			// allow for filter...
 			// $fields-values = array('fieldname'=>'value');
@@ -111,20 +158,21 @@ abstract class moniKey implements noSqlCRUD{
 		}
 		
 		elseif($names[0] == 'remove' && isset($arguments[0]) ){
+			
 			// support OPT's for delete in range ? not for now...
 			// if arguments[0] just pass it into the remove function
 			// of arguments[1] then process the option array like ("justOne") etc.
-			if(isset($arguments[0]['_id']) )
-				$collection->remove( array('_id'=> new MongoId($arguments[0]['_id'] ) ) ) ;
-			else{
-				$cursor = $collection->findOne( $arguments[0]);
-				foreach($cursor as $key=>$obj)
-					$r [$key] = $obj;
-			// get the doc then remove it .. annoying...
-			$to_remove  = $collection->findOne($arguments[0]);
-			$collection->remove( $to_remove->_id ) ;
-
+			
+			
+			if(isset($arguments[0]['_id']) ){
+				$to_find = $arguments[0]['_id'];
+				
 			}
+			else{
+				$to_find = $arguments[0];
+				
+			}
+			$collection->remove( array('_id'=> new MongoId($to_find) ) );
 		}
 		else{
 			if(is_array($arguments[0]))
@@ -135,7 +183,7 @@ abstract class moniKey implements noSqlCRUD{
 				}
 			else
 				$id= $arguments[0];
-			if(isset($data) && $data != NULL && $id != NULL){
+			if(isset($data) && $data != NULL && isset($id) && $id != NULL){
 				if(isset($_POST['_id']) ){
 					array_pop($data);
 					// really need to determine wether an id is a mongo id or a string
@@ -152,9 +200,10 @@ abstract class moniKey implements noSqlCRUD{
 				if(empty($existing)){
 					// try looking it up as a non MongoID
 					$existing = $collection->findOne(array('_id'=> $id ) );
-				}
+				}else{
 
-				$id = array_shift($existing);
+					$id = array_shift($existing);
+				}
 
 				// order could throw this operation off... annoying since array_dif doesnt sem to work ?
 				if(	!empty($data)){
@@ -162,7 +211,7 @@ abstract class moniKey implements noSqlCRUD{
 				// function/class for versioning and testing
 				// could get from arguments[0] but diddnt feel like making a new mongo object..
 						if(!$collection->update(array('_id'=> new MongoID($id)	),$data) ){
-							echo 'update failed';
+							$collection->save(array('_id'=> $id,$data));
 						}
 						// forward self to new page ? or handle with noClass...
 						return true;
@@ -173,7 +222,6 @@ abstract class moniKey implements noSqlCRUD{
 				// NO Change for existing record ??
 			}
 			elseif(isset($data) && is_array($data)){
-			
 				$collection = $collection->save($data);					
 			}elseif($id != NULL){
 			// count string and if it does not contain underscores...
@@ -207,11 +255,16 @@ abstract class moniKey implements noSqlCRUD{
 
 	}
 	public function put($data,$id=NULL,$database=NULL){
-		
-//		echo 'attempting to put';
+		// ID NOT STORING GREAT.
 		if($database == null) $database = get_called_class();
 		// check to see if document exists before putting or else replace to ensue.. maybe mongodb command exists to do this already
-		!$data['_id'] && $id != NULL AND $data['_id'] = $id;
+		if($id != null){
+			!$data['_id'] && $id != NULL AND $data['_id'] = $id;
+			$data2 = array('_id' => $data['_id']);
+			unset($data['_id']);
+			$data2 = array_merge($data2,$data);
+		}
+		
 		if(!$data['_id'] && $id == NULL && isset($this->_k) ){
 		// generate doc ... good time to use a _k to look up if its a unique key.. or use a _u to track unique
 		// keys per class ??
@@ -253,14 +306,16 @@ abstract class moniKey implements noSqlCRUD{
 			}
 			elseif(isset($_POST['_id'])) {
 				echo 'duplicate encountered';
+	
 				foreach($_POST as $key=>$value){
 					if($value == $id)
 						unset($_POST[$key]);
 				}
+				// resultant form has no container etc...
 				echo $this();
 				// this gets called prematurely ... only execute this if the post method is get?
 				// we arent handling this flow very well..
-				die( "$id already exists within '". get_called_class() . "', please pick a new value");
+				//die( "$id already exists within '". get_called_class() . "', please pick a new value");
 			}
 		}	
 
@@ -276,6 +331,28 @@ abstract class moniKey implements noSqlCRUD{
 				$search = array("$name"=> array('$exists' => true));
 				return self::$call($search );
 				}
+				
+			if(is_string($opt) && $opt != 'findOne' && $opt != 'findFirstField'){
+				// implement comma seperate list for opt to do multiple return fields
+				if($opts = explode(' ',trim($opt)) && count($opts) >1){
+					$opt=array();
+					foreach($opts as $the_field)
+					// use array map + lambada
+						$opt[$the_field]=true;
+					//return(self::$call(,$opt));	
+					
+				}else{
+					$opt = array();
+					foreach(self::$call(array("$name"=> $key),array($opt=>true)) as $result=>$array_val){
+						$opt[$result] = $array_val[key ($array_val)];
+					}
+					return $opt;
+					// this is to return a simplier data structure since t s only one field present
+//					return $call[key($call)];
+				}
+				return self::$call(array("$name"=> $key),$opt);
+				
+				}	
 			}	
 		
 		if(!is_array($name) && is_string($key) ){
@@ -294,19 +371,19 @@ abstract class moniKey implements noSqlCRUD{
 			return self::$call($find_2 );
 		}
 		// need to do this better. ... 
-		if(is_array($name_cpy)){
+		if(is_array($key) && is_array($name)){
+
+			//echo 'here';
+			return self::$call($name,$key);
+			}
+		elseif(is_array($name_cpy) && $opt == null && !is_array($key)){
 			if($opt == 'findOne' || $opt == 'findFirstField'){
 			// use view($criteria,NULL,NULL,'findOne')
 				$call = "findOne__$database";
 			}
 			return self::$call($name_cpy);
-		}
-		
-		if($opt == 'all keys')
+		}elseif($opt == 'all keys')
 			return self::$call($name);
-		elseif(is_array($key) && is_array($name)){
-			return self::$call($name,$key);
-			}
 		elseif(is_array($name)){
 			return self::$call($name);
 		}
@@ -319,7 +396,6 @@ abstract class moniKey implements noSqlCRUD{
 	// yikes update is always set to 'true' ? so we always create new records ? whats up with that...
 		$database == NULL AND $database = get_called_class();
 		$call = "__$database";
-		echo "\n\n update call : $call : $id\n\n";
 		return self::$call($id,$data);
 		
 	}
@@ -327,7 +403,6 @@ abstract class moniKey implements noSqlCRUD{
 		if($database ==null) $database = get_called_class();
 		$call = "remove__$database";
 		$id = array('_id'=>$id);
-		self::$call($id );
+		return self::$call($id );
 	}
-
 }
